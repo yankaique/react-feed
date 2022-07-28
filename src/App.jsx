e60aaeb3fd324dcbf8bbef2ff95b81201640a2d0
Yan Marinho <yan.marinho@pixter.com.br>
2022-07-28 08:07:34 -0300

    ✨ Insert CSS module

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
+import { Header } from "./components/Header";
 import { Post } from "./Post";
+
 function App() {
   return (
     <div>
+      <Header />
       <Post
         author="Yan"
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
