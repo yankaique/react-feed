d7b1aedbf501b8b24ad1928057e44f2f41e16bb7
Yan Marinho <yan.marinho@pixter.com.br>
2022-07-28 08:41:51 -0300

    💄 create header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,10 @@
 import styles from "./Header.module.css";
+import igniteLogo from "../assets/ignite-logo.svg";
 
 export function Header() {
   return (
     <header className={styles.header}>
+      <img src={igniteLogo} alt="Logotipo ignite" />
       <strong>Ignite feed</strong>
     </header>
   );
