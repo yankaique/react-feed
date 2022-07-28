6fd855397be2173e383c39d70b163bb2d995570c
Yan Marinho <yan.marinho@pixter.com.br>
2022-07-28 07:54:15 -0300

    ✨ initialize project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Post } from "./Post";
+function App() {
+  return (
+    <div>
+      <Post
+        author="Yan"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
+      />
+      <Post
+        author="Robinson"
+        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
+      />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Post.jsx
@@ -0,0 +1,8 @@
+export function Post({ author, content }) {
+  return (
+    <div>
+      <h1>{author}</h1>
+      <p>{content}</p>
+    </div>
+  );
+}
