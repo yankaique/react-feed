5f5e19c09fdbd3717d8309c59aacb70eeb7f7933
Yan Marinho <yan.marinho@pixter.com.br>
2022-07-29 08:48:28 -0300

    ✨ Starting sidebar page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,21 @@
 import { Header } from "./components/Header";
+import { Sidebar } from "./components/Sidebar";
 import { Post } from "./Post";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Yan"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
-      />
-      <Post
-        author="Robinson"
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
-      />
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+          <Post
+            author="Yan"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum illum ut dignissimos, doloremque eos aliquam expedita? Pariatur accusamus fugit quaerat voluptatibus reprehenderit, deleniti tenetur magni nobis nulla atque rerum dignissimos!"
+          />
+        </main>
+      </div>
     </div>
   );
 }
