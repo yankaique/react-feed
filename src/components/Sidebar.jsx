6629ef8db2052322c82262b286d8e69eb4a90e6d
Yan Marinho <yan.marinho@pixter.com.br>
2022-08-01 10:19:44 -0300

    💄 complete sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { PencilLine } from "phosphor-react";
 import styles from "./Sidebar.module.css";
 
 export function Sidebar() {
@@ -5,17 +6,23 @@ export function Sidebar() {
     <aside className={styles.sidebar}>
       <img
         src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=50"
-        alt="developer"
         className={styles.cover}
       />
 
       <div className={styles.profile}>
+        <img
+          className={styles.avatar}
+          src="https://avatars.githubusercontent.com/u/49956758?v=4"
+        />
         <strong>Yan Marinho</strong>
         <span>Web Developer</span>
       </div>
 
       <footer>
-        <a href="#">Editar seu perfil</a>
+        <a href="#">
+          <PencilLine size="20" />
+          Editar seu perfil
+        </a>
       </footer>
     </aside>
   );
