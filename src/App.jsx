5b54283b0c9aa0e3b59f518a73ef69f2b4262282
Yan Marinho <yan.marinho@pixter.com.br>
2022-08-04 09:00:56 -0300

    ✨ transform post in dinamic

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,38 @@ import { Sidebar } from "./components/Sidebar";
 import { Post } from "./components/Post";
 import styles from "./App.module.css";
 
+// author: { avatarUrl: "", name: "", role: ""}
+// publishedAt: Date
+// content: String
+
+const post = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://avatars.githubusercontent.com/u/49956758?v=4",
+      name: "Yan",
+      role: "Web Developer",
+    },
+    content: [
+      {
+        type: "paragraph",
+        content: "Fala galeraa 👋",
+      },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        type: "link",
+        content: "jane.design/doctorcare",
+      },
+    ],
+    hashtags: ["#novoprojeto", "#nlw", "#rocketseat"],
+    publishedAt: new Date("2022-08-03 11:11:00"),
+  },
+];
+
 function App() {
   return (
     <div>
@@ -10,8 +42,15 @@ function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {post.map((props) => (
+            <Post
+              key={props.id}
+              author={props.author}
+              content={props.content}
+              hashtags={props.hashtags}
+              publishedAt={props.publishedAt}
+            />
+          ))}
         </main>
       </div>
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,42 +1,71 @@
+import { useState } from "react";
+import { format, formatDistanceToNow } from "date-fns";
+import ptBR from "date-fns/locale/pt-BR";
+
 import { Avatar } from "./Avatar";
 import { Comment } from "./Comment";
 import styles from "./Post.module.css";
 
-export function Post() {
+export function Post({ author, publishedAt, content, hashtags }) {
+  const [comments, setComments] = useState([1, 2, 3]);
+
+  const publishedDateFormatted = format(
+    publishedAt,
+    "d 'de' LLLL 'as' HH:mm'h'",
+    {
+      locale: ptBR,
+    }
+  );
+
+  const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+    locale: ptBR,
+    addSuffix: true,
+  });
+
+  function handleCreateNewComment() {
+    event.preventDefault();
+    setComments([...comments, comments.length + 1]);
+  }
+
   return (
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <Avatar src="https://avatars.githubusercontent.com/u/49956758?v=4" />
+          <Avatar src={author.avatarUrl} />
           <div className={styles.authorInfo}>
-            <strong>Yan Marinho</strong>
-            <span>Web Developer</span>
+            <strong>{author.name}</strong>
+            <span>{author.role}</span>
           </div>
         </div>
 
-        <time title="1 de Agosto as 08:13" dateTime="2022-08-01 08:13:30">
-          Publicado há 1h
+        <time
+          title={publishedDateFormatted}
+          dateTime={publishedAt.toISOString()}
+        >
+          {publishedDateRelativeToNow}
         </time>
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
+        {content.map((line) => {
+          if (line.type === "paragraph") {
+            return <p>{line.content}</p>;
+          } else if (line.type === "link") {
+            return (
+              <p>
+                👉 <a href="">{line.content}</a>
+              </p>
+            );
+          }
+        })}
 
         <p>
-          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
-          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
-        </p>
-
-        <p>
-          👉 <a href="">jane.design/doctorcare</a>
-        </p>
-
-        <p>
-          <a href="">#novoprojeto</a> <a href="">#nlw </a>{" "}
-          <a href="">#rocketseat</a>
+          {hashtags.map((hashtags) => (
+            <a href="">{hashtags} </a>
+          ))}
         </p>
       </div>
-      <form className={styles.commentForm}>
+      <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
         <strong>Deixe seu feedback</strong>
         <textarea placeholder="Escreva um comentário..."></textarea>
         <footer>
@@ -44,9 +73,9 @@ export function Post() {
         </footer>
       </form>
       <div className={styles.commentList}>
-        <Comment />
-        <Comment />
-        <Comment />
+        {comments.map(() => (
+          <Comment />
+        ))}
       </div>
     </article>
   );
